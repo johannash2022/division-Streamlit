ab5daeba848af50146831db3898484c794fbb3ca
johannash2022 <119521576+johannash2022@users.noreply.github.com>
2022-11-30 21:55:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,10 @@ def main():
   st.markdown(html_temp,unsafe_allow_html=True)
   num1 = st.number_input("Number 1")
   num2 = st.number_input("Number 2")
-  result = num1 - num2
+  if(num1==0 or num2==0):
+    result = 0
+  else:
+    result=num1/num2
   st.success('The output is {}'.format(result))
   if st.button("Made By"):
       st.text("Parth Bansal")
